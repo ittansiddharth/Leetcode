f1ea0de1f26ace7602423981bcf8e57fa6b82557
ittansiddharth <sidy2310@live.com>
2018-04-09 14:59:48 +0530

    Add files via upload

---
+++ LongestSubString.cpp
@@ -0,0 +1,30 @@
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+SOLUTION
+
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256,-1);
+        int mlength = 0 , start = -1;
+        for(int i = 0; i < s.length() ; i++){
+            if(dict[s[i]] > start){
+                start = dict[s[i]];
+            }
+            dict[s[i]] = i;
+            mlength = max(mlength , i - start);
+        }
+            return mlength;
+    }
+};
